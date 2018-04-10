39c1052b0947241dd652add1dd6fe490007e4bc2
dimon7761 <dmitriy.litvin@dlnet.kharkov.com>
2018-04-10 15:41:23 +0300

    Update

--- vm2.sh
+++ vm2.sh
@@ -16,12 +16,12 @@ source /etc/network/interfaces.d/*
 
 # The loopback network interface
 auto lo
-iface lo inet loopback;
+iface lo inet loopback
 ' > $IF_CFG
 
 #INTERNAL
 echo '# Internal interface' >> $IF_CFG
-echo "auto  $INTERNAL_IF
+echo "auto $INTERNAL_IF
 iface $INTERNAL_IF inet static
 address $INTERNAL_IP
 gateway $GW_IP
@@ -29,9 +29,10 @@ gateway $GW_IP
 
 #INTERNAL VLAN
 modprobe 8021q
+grep -q 8021q /etc/modules || echo 8021q >> /etc/modules
 vconfig add $INTERNAL_IF $VLAN >> /dev/null 2>&1
 echo '# Internal interface vlan' >> $IF_CFG
-echo "auto  $INTERNAL_IF.$VLAN
+echo "auto $INTERNAL_IF.$VLAN
 iface $INTERNAL_IF.$VLAN inet static
 address $APACHE_VLAN_IP
 " >> $IF_CFG
